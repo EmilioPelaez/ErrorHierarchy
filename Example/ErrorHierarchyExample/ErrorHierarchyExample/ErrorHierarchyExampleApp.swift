0cca094ca1aca26c990e8e954372a8a1a2baf8f5
Emilio Peláez <me@epelaez.net>
2021-12-30 10:14:11 +0100

    Example Project

---
+++ Example/ErrorHierarchyExample/ErrorHierarchyExample/ErrorHierarchyExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ErrorHierarchyExampleApp.swift
+//  ErrorHierarchyExample
+//
+//  Created by Emilio Peláez on 30/12/21.
+//
+
+import SwiftUI
+
+@main
+struct ErrorHierarchyExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
